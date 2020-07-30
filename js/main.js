25642fd2741f7bb7e138937aa2db12b9e78a75a3
rusakovsb <rusakov@inbox.ru>
2020-07-30 12:50:50 +0300

    updated discount function and styles

--- js/main.js
+++ js/main.js
@@ -34,36 +34,20 @@
         $("#popup").fadeOut();
       });
 
-      if ($(".catalog-item__discount").is(":empty")) {
-        $(".catalog-item__old-price, .catalog-item__new-price").hide();
-      }
-      else {
-        $(".catalog-item__price").hide();
-      }
-
-      if ($(".product-card__discount").is(":empty")) {
-        $(".product-card__old-price, .product-card__new-price").hide();
-      }
-      else {
-        $(".product-card__price").hide();
-      }
-
-      $(".catalog-item").each(function() { 
-        var oldPrice = Number($(this).find(".catalog-item__old-price-value").text());
-        var discount = Number($(this).find(".catalog-item__discount").text());
+      $(".catalog-item, .product-info").each(function() { 
+        var oldPrice = Number($(this).find(".old-price-value").text());
+        var discount = Number($(this).find(".discount").text());
         var newPrice = ((100 - discount) / 100) * oldPrice;
-        $(this).find(".catalog-item__new-price-value").prepend(newPrice.toFixed(2));        
+        $(this).find(".new-price-value").prepend(newPrice.toFixed(2));        
       }) 
+
+      if ($(".discount").is(":empty")) {
+        $(".old-price, .new-price, .discount").remove();
+      }
+      else {
+        $(".price").remove();
+      }
    
-      $(".product-card").each(function() { 
-        var productOldPrice = Number($(this).find(".product-card__old-price-value").text());
-        var productDiscount = Number($(this).find(".product-card__discount").text());
-        var productNewPrice = ((100 - productDiscount) / 100) * productOldPrice;
-        $(this).find(".product-card__new-price-value").prepend(productNewPrice.toFixed(2));        
-      }) 
-
-
-
     }
   };
 })(jQuery);
